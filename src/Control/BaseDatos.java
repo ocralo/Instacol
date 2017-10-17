0ab78d294740da41e2b1bd4841c3b724661fb6d4
rodrigoescobarlopez <rodrigoescobarlopez@localhost>
2017-10-17 13:20:25 -0500

    puro momo marica

--- src/Control/BaseDatos.java
+++ src/Control/BaseDatos.java
@@ -42,8 +42,11 @@ public class BaseDatos {
      */
     public boolean crearConexion() {
         try {
+           String usuario = "crud";
+            String clave = "12345";
+            String bd = "insta_col";
             Class.forName("com.mysql.jdbc.Driver");                                      //user  //pass
-            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/insta_col", "InstaColAdmin", "instacoladmin");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + bd, usuario, clave);
             st = conexion.createStatement();
         } catch (SQLException ex) {
             ex.printStackTrace();
